3ae01f8bb7a279031f18cfe48cf8e5642906b5ac
Erin Black <erinblackdesign@gmail.com>
2017-09-07 19:31:21 -0500

    changed to constructor function

--- dice.js
+++ dice.js
@@ -1,7 +1,17 @@
-const dice = {
-  sides: 6,
-  roll:function(){
-    const randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
-  } //end roll
-}; //end dice
+// const dice = {
+//   sides: 6,
+//   roll:function(){
+//     const randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//     return randomNumber;
+//   } //end roll
+// }; //end dice
+
+function Dice(sides) {
+    this.sides = sides;
+    this.roll = function(){
+      const randomNumber = Math.floor(Math.random() * this.sides) + 1;
+        return randomNumber;
+    }; //end this.roll
+}// end Dice constructor function
+
+var dice = new Dice(6);
